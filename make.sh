07dbe5848438fe1c446ad0f517fa4a3ac0e9af38
Piskulich <piskuliche@ku.edu>
2017-03-20 13:50:01 -0500

    Updated makefile

--- make.sh
+++ make.sh
@@ -5,10 +5,13 @@
 
 FINITE_PATH=/panfs/pfs.local/work/thompson/e924p726/Programs
 
+echo "Welcome to the Python Code Collection: Unpacking"
+
 # Creates the Python Directory if it doesn't exist.
 if [ ! -d "$FINITE_PATH/Python" ]; then
     mkdir $FINITE_PATH/Python
     mkdir $FINITE_PATH/Python/Executable $FINITE_PATH/Python/Not-Executable
+    echo "$FINITE_PATH/Python not found - Creating."
 fi
 
 # CLEARS THE FOLDER
@@ -17,6 +20,23 @@ rm $FINITE_PATH/Python/* $FINITE_PATH/Python/Executable/* $FINITE_PATH/Python/No
 # Copies the programs into the proper directory
 # Executable Programs
 cp Block-Average/calculate_average.py $FINITE_PATH/Python/Executable
-cp 
+cp MSD-TO-D/MSD_to_D.py $FINITE_PATH/Python/Executable
+cp Minimization/minimize.py $FINITE_PATH/Python/Executable
+
 # Not-Executable Programs
 cp Curve-Fitting/calculate_fit.py $FINITE_PATH/Python/Not-Executable
+
+
+# Creates a New Bash Code Chunk
+if [ -e "~/.bash_PCC" ]; then
+    rm "~/.bash_PCC"
+fi
+
+echo 'export PATH="'$FINITE_PATH'/Python/Executable:$PATH"' > ~/.bash_PCC
+
+# Append to BashRC
+if ! grep -q PCCFLAG ~/.bashrc ; then
+    cat Src/brc_mod >> ~/.bashrc
+fi 
+
+echo "Python Code Collection: Unpack Complete"
