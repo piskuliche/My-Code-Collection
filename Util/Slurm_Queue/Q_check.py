f0e80ae7bb905822b97720de14eec854781b35f0
Zeke Piskulich <piskuliche@ku.edu>
2020-02-05 14:45:44 -0600

    Added more functions

--- Util/Slurm_Queue/Q_check.py
+++ Util/Slurm_Queue/Q_check.py
@@ -16,13 +16,19 @@ if args.n == 1:
 
 print(uniquename,user)
 
-lines=os.popen('squeue -u "%s"' % user).read()
+lines=os.popen('squeue -u "%s"' % user).read().split('\n')
+lines.pop()
+print(len(lines))
+
 job_printed=[]
 names_printed=[]
-for line in lines.split('\n'):
+running=[]
+pending=[]
+start=[]
+for line in lines:
     if "JOBID" in line:
-        print(line)
-    elif "R" in line:
+        start=line
+    elif "R" in line.split()[4]:
         jid=line.split()[0]
         if "_" in jid:
             job=jid.split('_')[0]
@@ -31,9 +37,41 @@ for line in lines.split('\n'):
                 if uniquename is True:
                     name=line.split()[2]
                     if name not in names_printed:
-                        print(line)
+                        running.append(line)
                         names_printed.append(name)
                 else:
-                    print(line)
+                    running.append(line)
+        else:
+            running.append(line)
+            job_printed.append(jid)
+    elif "PD" in line.split()[4]:
+        jid=line.split()[0]
+        if "_" in jid:
+            job=jid.split('_')[0]
+            if job not in job_printed:
+                job_printed.append(job)
+                if uniquename is True:
+                    name=line.split()[2]
+                    if name not in names_printed:
+                        pending.append(line)
+                        names_printed.append(name)
+                else:
+                    pending.append(line)
+        else:
+            pending.append(line)
+            job_printed.append(jid)
 
+print("----------------")
+print("    RUNNING     ")
+print("----------------")
+print(start)
+for job in running:
+    print(job)
+print("----------------")
+print("    PENDING     ")
+print("----------------")
+print(start)
+for job in pending:
+    print(job)
+    
 
