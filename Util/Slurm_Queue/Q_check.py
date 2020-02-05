3c215db32eb8f12a08afbcd97dbea7bb133f6865
Zeke Piskulich <piskuliche@ku.edu>
2020-02-05 12:07:32 -0600

    Updated to involve args

--- Util/Slurm_Queue/Q_check.py
+++ Util/Slurm_Queue/Q_check.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python
 import os
+import argparse
 
-lines=os.popen('squeue -u "e924p726"').read()
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-u', default='e924p726', type=str, help='Username')
+parser.add_argument('-n', default=0, type=int, help='Print only unique names')
+args = parser.parse_args()
+
+user=args.u
+uniquename = False
+if args.n == 1:
+    uniquename=True
+
+
+print(uniquename,user)
+
+lines=os.popen('squeue -u "%s"' % user).read()
 job_printed=[]
+names_printed=[]
 for line in lines.split('\n'):
     if "JOBID" in line:
         print(line)
@@ -11,7 +27,13 @@ for line in lines.split('\n'):
         if "_" in jid:
             job=jid.split('_')[0]
             if job not in job_printed:
-                print(line)
                 job_printed.append(job)
+                if uniquename is True:
+                    name=line.split()[2]
+                    if name not in names_printed:
+                        print(line)
+                        names_printed.append(name)
+                else:
+                    print(line)
 
 
